cb9c8d8bb14935456fc3b5e58659355108dab651
Alua Baikadamova <abaikad00@gmail.com>
2019-02-15 04:33:17 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,5 +34,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "select series.title
+  from Series
+  join Characters
+  where characters.species='human' and characters.series_id=series.id
+  order by length(series.title) desc limit 1"
 end
