39e3548e9a5eff451e0cb7fa897f530dbbfcb7ff
Alua Baikadamova <abaikad00@gmail.com>
2019-02-15 04:55:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,9 +34,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "select characters.name
+  "select characters.name, count (characters.name)
   from characters
   left join character_books
   where characters.id=character_books.character_id
-  order by characters.name desc "
+   "
 end
